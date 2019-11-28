61f38c3470e3408d743a1e1818ff73ef14dece54
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2019-11-28 19:37:34 +0100

    CPP : Make methods virtual in Matrix Class

--- src/cpp/include/tbsla/cpp/Matrix.hpp
+++ src/cpp/include/tbsla/cpp/Matrix.hpp
@@ -9,7 +9,7 @@ namespace tbsla { namespace cpp {
 class Matrix {
   public:
     friend std::ostream & operator<<( std::ostream &os, const Matrix &m) { return m.print(os); };
-    std::vector<double> spmv(const std::vector<double> &v, int vect_incr = 0) const;
+    virtual std::vector<double> spmv(const std::vector<double> &v, int vect_incr = 0) const = 0;
     std::vector<double> & saxpy(const std::vector<double> &x, std::vector<double> &y);
 
     int const get_n_row() {return n_row;}
