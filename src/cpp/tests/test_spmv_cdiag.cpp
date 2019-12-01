663680abb9b937968a807cde731d2665a0893338
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2019-12-01 18:27:26 +0100

    CPP : Add more tests for spmv with CDiag matrices

--- src/cpp/tests/test_spmv_cdiag.cpp
+++ src/cpp/tests/test_spmv_cdiag.cpp
@@ -60,6 +60,18 @@ int main(int argc, char** argv) {
     std::cout << "=== test " << t++ << " ===" << std::endl;
     test_cdiag(10, 5, i);
   }
+  for(int i = 0; i <= 12; i++) {
+    std::cout << "=== test " << t++ << " ===" << std::endl;
+    test_cdiag(30, 30, 2 * i);
+  }
+  for(int i = 0; i <= 12; i++) {
+    std::cout << "=== test " << t++ << " ===" << std::endl;
+    test_cdiag(20, 30, 2 * i);
+  }
+  for(int i = 0; i <= 12; i++) {
+    std::cout << "=== test " << t++ << " ===" << std::endl;
+    test_cdiag(30, 20, 2 * i);
+  }
 
   std::cout << "=== finished without error === " << std::endl;
 }
