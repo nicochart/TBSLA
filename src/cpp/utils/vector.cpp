153ea4be704c5fffead82956cc4e0942894484a4
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2019-12-01 18:19:09 +0100

    CPP : Fix and Simplify function to check results of spmv with a CDiag matrix

--- src/cpp/utils/vector.cpp
+++ src/cpp/utils/vector.cpp
@@ -1,95 +1,27 @@
 #include <tbsla/cpp/utils/vector.hpp>
 
+int check(int i, double v, double exp, int return_value, bool debug) {
+  if(debug)
+    std::cout << "i = " << i << " r[i] = " << v << " exp " << exp << std::endl;
+  if(v != exp) {
+    return return_value;
+  }
+  return 0;
+}
+
 int tbsla::utils::vector::test_vres_cdiag(int nr, int nc, int c, std::vector<double> r, bool debug) {
-  int i = 0;
-  if( c == 0) {
-    for(; i < std::min(nc, nr); i++) {
-      if(debug)
-        std::cout << "i = " << i << " r[i] = " << r[i] << " exp " << i << std::endl;
-      if(r[i] != i) {
-        return 10;
-      }
-    }
-    for(; i < nr; i++) {
-      if(debug)
-        std::cout << "i = " << i << " r[i] = " << r[i] << " exp " << 0 << std::endl;
-      if(r[i] != 0) {
-        return 11;
-      }
-    }
+  int i;
+  for(i = 0; i < std::min(c, nr); i++) {
+    int rv = check(i, r[i], i < nc - c ? i + c : 0, 10, debug);
+    if(rv) return rv;
   }
-  for(; i < std::min(c, nc - c); i++) {
-    if(debug)
-      std::cout << "i = " << i << " r[i] = " << r[i] << " exp " << i + c << std::endl;
-    if(r[i] != i + c) {
-      return 20;
-    }
+  for(; i < std::min(nr, nc - c); i++) {
+    int rv = check(i, r[i], c == 0 ? i : 2 * i, 12, debug);
+    if(rv) return rv;
   }
-  for(; i < std::min(c, nr); i++) {
-    if(debug)
-      std::cout << "i = " << i << " r[i] = " << r[i] << " exp " << 0 << std::endl;
-    if(r[i] != 0) {
-      return 21;
-    }
-  }
-  if(nr < nc - c) {
-    if(debug)
-      std::cout << "case nr < nc - c" << std::endl;
-    for(; i < nr; i++) {
-      if(debug)
-        std::cout << "i = " << i << " r[i] = " << r[i] << " exp " << 2 * i << std::endl;
-      if(r[i] != 2 * i) {
-        return 30;
-      }
-    }
-  } else if(nr < nc + c) {
-    if(debug)
-      std::cout << "case nr < nc + c" << std::endl;
-    for(; i < std::min(nr, nc) - c; i++) {
-      if(debug)
-        std::cout << "i = " << i << " r[i] = " << r[i] << " exp " << 2 * i << std::endl;
-      if(r[i] != 2 * i) {
-        return 40;
-      }
-    }
-    for(; i < std::min(nr, nc); i++) {
-      if(debug)
-        std::cout << "i = " << i << " r[i] = " << r[i] << " exp " << i - c << std::endl;
-      if(r[i] != i - c) {
-        return 41;
-      }
-    }
-    for(; i < nr; i++) {
-      if(debug)
-        std::cout << "i = " << i << " r[i] = " << r[i] << " exp " << i - c << std::endl;
-      if(r[i] != i - c) {
-        return 42;
-      }
-    }
-  } else {
-    if(debug)
-      std::cout << "case nr >= nc + c" << std::endl;
-    for(; i < std::min(nr, nc - c); i++) {
-      if(debug)
-        std::cout << "i = " << i << " r[i] = " << r[i] << " exp " << 2 * i << std::endl;
-      if(r[i] != 2 * i) {
-        return 50;
-      }
-    }
-    for(; i < std::min(nr, nc + c); i++) {
-      if(debug)
-        std::cout << "i = " << i << " r[i] = " << r[i] << " exp " << i - c << std::endl;
-      if(r[i] != i - c) {
-        return 51;
-      }
-    }
-    for(; i < nr; i++) {
-      if(debug)
-        std::cout << "i = " << i << " r[i] = " << r[i] << " exp " << 0 << std::endl;
-      if(r[i] != 0) {
-        return 52;
-      }
-    }
+  for(; i < nr; i++) {
+    int rv = check(i, r[i], i < nc + c ? i - c : 0, 14, debug);
+    if(rv) return rv;
   }
   if(i < nr) {
     return 222;
