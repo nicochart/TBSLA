d5520f0662a15a6e8a9f69d443dd62a7a6f77095
aamrani-dev <amine.amrani@polytech-lille.net>
2020-07-16 12:42:52 +0200

    fix some mpi bugs

--- src/cpp/tests/test_page_rank.cpp
+++ src/cpp/tests/test_page_rank.cpp
@@ -5,39 +5,31 @@
 #include <tbsla/cpp/utils/InputParser.hpp>
 
 int main(int argc, char** argv) {
-  	InputParser input(argc, argv);
-  	int n = 4; 
-	double epsilon = 0.00001; 
-	double beta = 1; 
-  	int max_iterations = 100; 
-  	if(input.has_opt("--beta")) {
-    	std::string beta_string = input.get_opt("--beta", "1");
-    	beta = std::stod(beta_string);
-  	}
-	if(input.has_opt("--epsilon")) {
-    	std::string epsilon_string = input.get_opt("--epsilon", "1");
-    	epsilon = std::stod(epsilon_string);
-  	}
+  InputParser input(argc, argv);
+  int n = 4; 
+  double epsilon = 0.0000001; 
+  double beta = 1; 
+  int max_iterations = 100; 
 
-  	if(input.has_opt("--max-iterations")) {	
-    	std::string max_iterations_string = input.get_opt("--max-iterations", "1");
-    	max_iterations = std::stoi(max_iterations_string);
-  	}
   	
-  	
-	std::vector<double> values{11.0, 12.0, 2.0, 6.0, 8.0,3.0, 13.0, 14.0, 16.0};
-	std::vector<int> columns{0, 1, 3, 1, 3, 3, 0, 2, 3};
-	std::vector<int> rows{0, 3, 5, 6, 9}; 
+  std::vector<double> values{11.0, 12.0, 2.0, 6.0, 8.0,3.0, 13.0, 14.0, 16.0};
+  std::vector<int> columns{0, 1, 3, 1, 3, 3, 0, 2, 3};
+  std::vector<int> rows{0, 3, 5, 6, 9}; 
+  std::vector<double> solution{0.5246, 0.4342, 0.1231, 1.0};
+  tbsla::cpp::Matrix * A = new tbsla::cpp::MatrixCSR(n, n, values, rows, columns);
+  A->print_as_dense(std::cout); 
 
-	tbsla::cpp::Matrix * A = new tbsla::cpp::MatrixCSR(n, n, values, rows, columns);
-	A->print_as_dense(std::cout); 
-	
 	std::vector<double> b(n); 
 	b = A->page_rank(epsilon, beta, max_iterations);
-	
-	std::cout << 	"SOLUTION ["; 
-	for(int  i = 0; i < n; i++){
-		std::cout << b[i] << ", " ; 
-	} 
-	std::cout << "]"<< std::endl ; 
+
+  double error = 0.0;
+  for(int i = 0 ; i < n; i++) {
+    error += std::abs(b[i] - solution[i]);
+  }
+  if(error > 0.01){
+    std::cout << "Test failed" << std::endl;
+    return 1; 
+  }
+  std::cout << "Test OK" << std::endl;
+  return 0; 
 }
--- src/mpi/apps/tbsla_page_rank_mpi.cpp
+++ src/mpi/apps/tbsla_page_rank_mpi.cpp
@@ -21,10 +21,10 @@ int main(int argc, char **argv){
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 		
   InputParser input(argc, argv);
-  	int n = 4;
-	double epsilon = 0.00001;
-	double beta = 1;
-  	int max_iterations = 100;
+  int n = 4;
+  double epsilon = 0.00001;
+  double beta = 1;
+  int max_iterations = 100;
 
   if(input.has_opt("--beta")) {
    	std::string beta_string = input.get_opt("--beta", "1");
@@ -33,53 +33,53 @@ int main(int argc, char **argv){
 	if(input.has_opt("--epsilon")) {
     	std::string epsilon_string = input.get_opt("--epsilon", "1");
     	epsilon = std::stod(epsilon_string);
-  	}
+  }
 
-  	if(input.has_opt("--max-iterations")) {	
-    	std::string max_iterations_string = input.get_opt("--max-iterations", "1");
-    	max_iterations = std::stoi(max_iterations_string);
-  	}
+  if(input.has_opt("--max-iterations")) {	
+   	std::string max_iterations_string = input.get_opt("--max-iterations", "1");
+   	max_iterations = std::stoi(max_iterations_string);
+ 	}
 	
 	std::string matrix_input = input.get_opt("--matrix_input");
-  	if(matrix_input == "") {
-  	  std::cerr << "A matrix file has to be given with the parameter --matrix_input file" << std::endl;
-    	exit(1);
-  	}
+  if(matrix_input == "") {
+    std::cerr << "A matrix file has to be given with the parameter --matrix_input file" << std::endl;
+   	exit(1);
+  }
 
 	std::string format = input.get_opt("--format");
-  	if(format == "") {
-    	std::cerr << "A file format has to be given with the parameter --format format" << std::endl;
-    	exit(1);
-  	}
+  if(format == "") {
+   	std::cerr << "A file format has to be given with the parameter --format format" << std::endl;
+   	exit(1);
+  }
     
 	std::string gr_string = input.get_opt("--GR", "1");
-  	std::string gc_string = input.get_opt("--GC", "1");
-  	int GR = std::stoi(gr_string);
-  	int GC = std::stoi(gc_string);
-  	if(world != GR * GC) {
-    	printf("The number of processes (%d) does not match the grid dimensions (%d x %d = %d).\n", world, GR, GC, GR * GC);
-    	exit(99);
-  	}
+  std::string gc_string = input.get_opt("--GC", "1");
+  int GR = std::stoi(gr_string);
+  int GC = std::stoi(gc_string);
+  if(world != GR * GC) {
+   	printf("The number of processes (%d) does not match the grid dimensions (%d x %d = %d).\n", world, GR, GC, GR * GC);
+   	exit(99);
+  }
 
 	tbsla::mpi::Matrix *m;
 
-  	if(format == "COO" | format == "coo") {
-    	m = new tbsla::mpi::MatrixCOO();
-    } else if(format == "SCOO" | format == "scoo") {
-    	m = new tbsla::mpi::MatrixSCOO();
-    } else if(format == "CSR" | format == "csr") {
-    	m = new tbsla::mpi::MatrixCSR();
-    } else if(format == "ELL" | format == "ell") {
-    	m = new tbsla::mpi::MatrixELL();
-    } else if(format == "DENSE" | format == "dense") {
-    	m = new tbsla::mpi::MatrixDENSE();
-    } else {
-    	if(rank == 0) {
-      		std::cerr << format << " unrecognized!" << std::endl;
-    	}
-    	exit(1);
-   	}  
-      
+  if(format == "COO" | format == "coo") {
+   	m = new tbsla::mpi::MatrixCOO();
+  } else if(format == "SCOO" | format == "scoo") {
+   	m = new tbsla::mpi::MatrixSCOO();
+  } else if(format == "CSR" | format == "csr") {
+   	m = new tbsla::mpi::MatrixCSR();
+  } else if(format == "ELL" | format == "ell") {
+   	m = new tbsla::mpi::MatrixELL();
+  } else if(format == "DENSE" | format == "dense") {
+   	m = new tbsla::mpi::MatrixDENSE();
+  } else {
+   	if(rank == 0) {
+   		std::cerr << format << " unrecognized!" << std::endl;
+    }
+   	exit(1);
+  }  
+
 	m->read_bin_mpiio(MPI_COMM_WORLD, matrix_input, rank / GC, rank % GC, GR, GC);
 
 	std::vector<double> b(m->get_n_col());
