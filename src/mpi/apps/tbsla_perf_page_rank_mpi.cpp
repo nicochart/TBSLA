d7b3c9542d354261951bcb088a2cee62069f35a1
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2020-08-05 00:18:00 +0200

    Rename pagerank application source file and initiate the change in their input parameter process

--- src/mpi/apps/tbsla_perf_page_rank_mpi.cpp
+++ src/mpi/apps/tbsla_perf_page_rank_mpi.cpp
@@ -25,39 +25,26 @@ int main(int argc, char** argv) {
   MPI_Comm_size(MPI_COMM_WORLD, &world);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  double epsilon = 0.00001;
-  double beta = 1;
-  int max_iterations = 100;
-  int nb_iterations_done;
-
-  if(input.has_opt("--beta")) {
-    std::string beta_string = input.get_opt("--beta", "1");
-    beta = std::stod(beta_string);
-  }
-  if(input.has_opt("--epsilon")) {
-      std::string epsilon_string = input.get_opt("--epsilon", "1");
-      epsilon = std::stod(epsilon_string);
-  }
-
-  if(input.has_opt("--max-iterations")) { 
-    std::string max_iterations_string = input.get_opt("--max-iterations", "1");
-    max_iterations = std::stoi(max_iterations_string);
-  }
-  
   std::string format = input.get_opt("--format");
   if(format == "") {
     std::cerr << "A file format has to be given with the parameter --format format" << std::endl;
     exit(1);
   }
 
-  std::string nr_string = input.get_opt("--NR", "1024");
-  std::string nc_string = input.get_opt("--NC", "1024");
+  std::string matrix_dim_string = input.get_opt("--matrix_dim", "1024");
 
   std::string gr_string = input.get_opt("--GR", "1");
   std::string gc_string = input.get_opt("--GC", "1");
 
-  int NR = std::stoi(nr_string);
-  int NC = std::stoi(nc_string);
+  std::string beta_string = input.get_opt("--beta", "0.85");
+  std::string epsilon_string = input.get_opt("--epsilon", "0.00001");
+  std::string max_iterations_string = input.get_opt("--max-iterations", "10000");
+
+  double epsilon = std::stod(epsilon_string);
+  double beta = std::stod(beta_string);
+  int max_iterations = std::stoi(max_iterations_string);;
+  int nb_iterations_done;
+  int matrix_dim = std::stoi(matrix_dim_string);
   int GR = std::stoi(gr_string);
   int GC = std::stoi(gc_string);
   int C = -1;
@@ -97,7 +84,7 @@ int main(int argc, char** argv) {
   }
   auto t_app_start = now();
 
-  m->fill_cqmat(NR, NC, C, Q, S, rank / GC, rank % GC, GR, GC);
+  m->fill_cqmat_stochastic(matrix_dim, matrix_dim, C, Q, S, rank / GC, rank % GC, GR, GC);
 
   if(input.has_opt("--print-infos")) {
     m->print_stats(std::cout);
@@ -117,15 +104,16 @@ int main(int argc, char** argv) {
     std::map<std::string, std::string> outmap;
     outmap["test"] = "page_rank";
     outmap["matrix_format"] = format;
-    outmap["n"] = std::to_string(m->get_n_col());
+    outmap["matrix_dim"] = std::to_string(m->get_n_col());
     outmap["g_row"] = gr_string;
     outmap["g_col"] = gc_string;
     outmap["nnz"] = std::to_string(m->get_gnnz());
     outmap["nb_iterations"] = std::to_string(nb_iterations_done);    
     outmap["time_app_in"] = std::to_string((t_app_end - t_app_start) / 1e9);
     outmap["time_op"] = std::to_string((t_op_end - t_op_start) / 1e9);
+    outmap["time_gen_mat"] = std::to_string((t_op_start - t_app_start) / 1e9);
     outmap["lang"] = "MPI";
-    outmap["matrix_type"] = "cqmat";
+    outmap["matrix_type"] = "cqmat_stochastic";
     outmap["cqmat_c"] = std::to_string(C);
     outmap["cqmat_q"] = std::to_string(Q);
     outmap["cqmat_s"] = std::to_string(S);
