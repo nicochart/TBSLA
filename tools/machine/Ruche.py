5a9cb1a6a282406804873496cb66ed1a14699eb0
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2020-10-26 18:34:50 +0100

    scripts : update Ruche environment to work for MPI apps with OpenMPI

--- tools/machine/Ruche.py
+++ tools/machine/Ruche.py
@@ -10,7 +10,7 @@ def get_sockets_per_node(args):
   return 1
 
 def get_mpirun(args):
-  return "mpirun "
+  return "srun "
 
 def get_mpirun_options_hpx(args):
   return f"-n {args.nodes}"
@@ -21,8 +21,14 @@ def get_submit_cmd(args):
 def get_env(args):
   env = """
 module purge
-module load hpx/1.4.0/intel-19.0.3.199-intel-mpi cmake/3.16.2/intel-19.0.3.199 intel/19.0.3/gcc-4.8.5 intel-mpi/2019.3.199/intel-19.0.3.199 python/3.7.6/intel-19.0.3.199
+#module load jemalloc/5.2.1/intel-19.0.3.199
+module load gcc/9.2.0/gcc-4.8.5
+#module load openmpi/4.0.2/gcc-9.2.0
+module load openmpi/3.1.5/gcc-9.2.0
+module load cmake/3.16.2/intel-19.0.3.199
+module load python/3.7.6/intel-19.0.3.199
 export PATH=$PATH:${HOME}/install/tbsla/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${HOME}/install/hpx/2b91d44/release/lib64:${HOME}/install/boost/1.69.0/release/lib:${HOME}/install/hwloc/2.0.4/lib:${HOME}/install/tbsla/lib:${HOME}/install/tbsla/lib64
 """
   return env
 
@@ -32,9 +38,9 @@ def get_header(args):
 
   header = f"""\
 #!/bin/bash
-#SBATCH -p cpu_short
+#SBATCH -p cpu_med
 #SBATCH --nodes={args.nodes}
-#SBATCH --cpus-per-task={get_cores_per_node(args)}
+#SBATCH --ntasks-per-node={get_cores_per_node(args)}
 #SBATCH --exclusive
 #SBATCH --output logs/tbsla_%x.%J.out
 
@@ -59,3 +65,4 @@ def post_processing(args):
 def post_run_cmd(args):
   s = ""
   return s
+
