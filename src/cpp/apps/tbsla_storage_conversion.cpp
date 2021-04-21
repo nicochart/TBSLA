1ef2c647dc5283e9fd77f3c1ca66aed63a0e1ca5
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2021-04-21 20:36:26 +0200

    CPP : Improve conversion app to support the other formats

--- src/cpp/apps/tbsla_storage_conversion.cpp
+++ src/cpp/apps/tbsla_storage_conversion.cpp
@@ -1,22 +1,60 @@
-#include <tbsla/cpp/MatrixCOO.hpp>
+#include <tbsla/cpp/Matrix.hpp>
+#include <tbsla/cpp/MatrixDENSE.hpp>
 #include <tbsla/cpp/MatrixCSR.hpp>
+#include <tbsla/cpp/MatrixELL.hpp>
+#include <tbsla/cpp/MatrixCOO.hpp>
+#include <tbsla/cpp/MatrixSCOO.hpp>
+#include <tbsla/cpp/utils/InputParser.hpp>
 
 #include <fstream>
 
 
 int main(int argc, char** argv) {
+  InputParser parser(argc, argv);
 
-  if(argc == 3) {
-    std::ifstream is(std::string(argv[1]), std::ifstream::binary);
-    tbsla::cpp::MatrixCOO mcoo;
-    mcoo.read(is);
-    is.close();
-    mcoo.print_stats(std::cout);
-    mcoo.print_infos(std::cout);
-
-    tbsla::cpp::MatrixCSR mcsr(mcoo);
-    std::ofstream os(std::string(argv[2]), std::ofstream::binary);
-    mcsr.write(os);
-    os.close();
+  std::string input = parser.get_opt("--input");
+  if(input == "") {
+    std::cerr << "An input file containing COO matrix has to be given with the parameter --input input" << std::endl;
+    exit(1);
   }
+
+  std::string output = parser.get_opt("--output");
+  if(output == "") {
+    std::cerr << "An output file has to be given with the parameter --output output" << std::endl;
+    exit(1);
+  }
+
+  std::string format = parser.get_opt("--format");
+  if(format == "") {
+    std::cerr << "A matrix format for the output has to be given with the parameter --format format" << std::endl;
+    exit(1);
+  }
+
+  std::ifstream is(input, std::ifstream::binary);
+  tbsla::cpp::MatrixCOO mcoo;
+  mcoo.read(is);
+  is.close();
+  mcoo.print_stats(std::cout);
+  mcoo.print_infos(std::cout);
+
+  tbsla::cpp::Matrix *m;
+
+  if(format == "SCOO" | format == "scoo") {
+    m = new tbsla::cpp::MatrixSCOO(mcoo);
+  } else if(format == "CSR" | format == "csr") {
+    m = new tbsla::cpp::MatrixCSR(mcoo);
+  } else if(format == "ELL" | format == "ell") {
+    m = new tbsla::cpp::MatrixELL(mcoo);
+  } else if(format == "DENSE" | format == "dense") {
+    m = new tbsla::cpp::MatrixDENSE(mcoo);
+  } else {
+    std::cerr << format << " unrecognized!" << std::endl;
+    exit(1);
+  }
+
+
+  m->print_infos(std::cout);
+  std::ofstream os(output, std::ofstream::binary);
+  m->write(os);
+  os.close();
 }
