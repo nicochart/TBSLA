27f2ba61544c084c9e66346356688f54b2d426d8
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2021-06-14 15:05:47 +0900

    Rewrite cpp/apps/tbsla_perf to cimprove the performances

--- src/cpp/apps/tbsla_perf.cpp
+++ src/cpp/apps/tbsla_perf.cpp
@@ -24,34 +24,8 @@ static std::uint64_t now() {
   return static_cast<std::uint64_t>(ns.count());
 }
 
-int main(int argc, char** argv) {
-  InputParser input(argc, argv);
-
-#if TBSLA_COMPILED_WITH_OMP
-  //https://www.nics.tennessee.edu/files/tutorials/HybridHello/HybridHello.c
-  if(input.has_opt("--report-affinity")) {
-    int thread;
-    cpu_set_t coremask;
-    char clbuf[7 * CPU_SETSIZE], hnbuf[64];
-    memset(clbuf, 0, sizeof(clbuf));
-    memset(hnbuf, 0, sizeof(hnbuf));
-    (void)gethostname(hnbuf, sizeof(hnbuf));
-    #pragma omp parallel private(thread, coremask, clbuf)
-    {
-      thread = omp_get_thread_num();
-      (void)sched_getaffinity(0, sizeof(coremask), &coremask);
-      tbsla::cpp::utils::cpuset_to_cstr(&coremask, clbuf);
-      #pragma omp barrier
-      printf("Hello from thread %d, on %s. (core affinity = %s)\n", thread, hnbuf, clbuf);
-    }
-  }
-#endif
-
-  std::string format = input.get_opt("--format");
-  if(format == "") {
-    std::cerr << "A file format has to be given with the parameter --format format" << std::endl;
-    exit(1);
-  }
+template <class T>
+void dothings(InputParser& input, std::string &format) {
 
   std::string nr_string = input.get_opt("--NR", "1024");
   std::string nc_string = input.get_opt("--NC", "1024");
@@ -89,70 +63,70 @@ int main(int argc, char** argv) {
     exit(1);
   }
 
-  tbsla::cpp::Matrix *m;
-
-  if(format == "COO" | format == "coo") {
-    m = new tbsla::cpp::MatrixCOO();
-  } else if(format == "SCOO" | format == "scoo") {
-    m = new tbsla::cpp::MatrixSCOO();
-  } else if(format == "CSR" | format == "csr") {
-    m = new tbsla::cpp::MatrixCSR();
-  } else if(format == "ELL" | format == "ell") {
-    m = new tbsla::cpp::MatrixELL();
-  } else if(format == "DENSE" | format == "dense") {
-    m = new tbsla::cpp::MatrixDENSE();
-  } else {
-    std::cerr << format << " unrecognized!" << std::endl;
-    exit(1);
-  }
+  T m;
   auto t_app_start = now();
-
-
   if(matrix == "cdiag") {
-    m->fill_cdiag(NR, NC, C, 0, 0, 1, 1);
+    m.fill_cdiag(NR, NC, C, 0, 0, 1, 1);
   } else if(matrix == "cqmat") {
-    m->fill_cqmat(NR, NC, C, Q, S, 0, 0, 1, 1);
+    m.fill_cqmat(NR, NC, C, Q, S, 0, 0, 1, 1);
   } else {
     std::ifstream is(matrix_folder + "/" + matrix + "." + format, std::ifstream::binary);
-    m->read(is);
+    m.read(is);
     is.close();
   }
 
   if(input.has_opt("--numa-init")) {
-    m->NUMAinit();
+    m.NUMAinit();
   }
 
   std::random_device rnd_device;
   std::mt19937 mersenne_engine {rnd_device()};  // Generates random integers
   std::uniform_real_distribution<double> dist {-1, 1};
   auto gen = [&dist, &mersenne_engine](){ return dist(mersenne_engine); };
-  std::vector<double> vec(m->get_n_col());
-  std::vector<double> res(m->get_n_row());
-  std::generate(begin(vec), end(vec), gen);
+  double* vec = new double[m.get_n_col()]();
+  double* res = new double[m.get_n_row()]();
+  std::generate(vec, vec + m.get_n_col(), gen);
 
   auto t_op_start = now();
+  double t_op = 0;
+  int ITERATIONS = 100;
   if(op == "spmv" or op == "spmv_no_redist") {
-    double* res = m->spmv(vec.data());
+    for(int i = 0; i < ITERATIONS; i++) {
+      t_op_start = now();
+      double* res = m.spmv(vec);
+      t_op += now() - t_op_start;
+      delete[] res;
+    }
   } else if(op == "Ax" or op == "Ax_") {
-    m->Ax(res.data(), vec.data());
+    t_op_start = now();
+    for(int i = 0; i < ITERATIONS; i++) {
+      m.Ax(res, vec);
+    }
+    t_op = now() - t_op_start;
   } else if(op == "a_axpx") {
-    double* res = m->a_axpx_(vec.data());
+    for(int i = 0; i < ITERATIONS; i++) {
+      t_op_start = now();
+      double* res = m.a_axpx_(vec);
+      t_op += now() - t_op_start;
+      delete[] res;
+    }
   }
-  auto t_op_end = now();
   auto t_app_end = now();
+  delete[] res;
+  delete[] vec;
 
   std::map<std::string, std::string> outmap;
   outmap["test"] = op;
   outmap["matrix_format"] = format;
-  outmap["n_row"] = std::to_string(m->get_n_row());
-  outmap["n_col"] = std::to_string(m->get_n_col());
-  outmap["nnz"] = std::to_string(m->get_nnz());
+  outmap["n_row"] = std::to_string(m.get_n_row());
+  outmap["n_col"] = std::to_string(m.get_n_col());
+  outmap["nnz"] = std::to_string(m.get_nnz());
   outmap["time_app_in"] = std::to_string((t_app_end - t_app_start) / 1e9);
-  outmap["time_op"] = std::to_string((t_op_end - t_op_start) / 1e9);
+  outmap["time_op"] = std::to_string(t_op / 1e9 / ITERATIONS);
   if(op == "spmv" or op == "spmv_no_redist" or op == "Ax" or op == "Ax_") {
-    outmap["gflops"] = std::to_string(2.0 * m->get_nnz() / (t_op_end - t_op_start));
+    outmap["gflops"] = std::to_string(2.0 * m.get_nnz() / t_op * ITERATIONS);
   } else if(op == "a_axpx") {
-    outmap["gflops"] = std::to_string(4.0 * m->get_nnz() / (t_op_end - t_op_start));
+    outmap["gflops"] = std::to_string(4.0 * m.get_nnz() / t_op * ITERATIONS);
   }
 #if TBSLA_COMPILED_WITH_OMP
   outmap["lang"] = "CPPOMP";
@@ -189,5 +163,49 @@ int main(int argc, char** argv) {
     std::cout << ",\"" << it->first << "\":\"" << it->second << "\"";
   }
   std::cout << "}\n";
+}
+
+
+int main(int argc, char** argv) {
+  InputParser input(argc, argv);
+#if TBSLA_COMPILED_WITH_OMP
+  if(input.has_opt("--report-affinity")) {
+    int thread;
+    cpu_set_t coremask;
+    char clbuf[7 * CPU_SETSIZE], hnbuf[64];
+    memset(clbuf, 0, sizeof(clbuf));
+    memset(hnbuf, 0, sizeof(hnbuf));
+    (void)gethostname(hnbuf, sizeof(hnbuf));
+    #pragma omp parallel private(thread, coremask, clbuf)
+    {
+      thread = omp_get_thread_num();
+      (void)sched_getaffinity(0, sizeof(coremask), &coremask);
+      tbsla::cpp::utils::cpuset_to_cstr(&coremask, clbuf);
+      #pragma omp barrier
+      printf("Hello from thread %d, on %s. (core affinity = %s)\n", thread, hnbuf, clbuf);
+    }
+  }
+#endif
+  std::string format = input.get_opt("--format");
+  if(format == "") {
+    std::cerr << "A file format has to be given with the parameter --format format" << std::endl;
+    exit(1);
+  }
+
+  if(format == "COO" | format == "coo") {
+    dothings<tbsla::cpp::MatrixCOO>(input, format);
+  } else if(format == "SCOO" | format == "scoo") {
+    dothings<tbsla::cpp::MatrixSCOO>(input, format);
+  } else if(format == "CSR" | format == "csr") {
+    dothings<tbsla::cpp::MatrixCSR>(input, format);
+  } else if(format == "ELL" | format == "ell") {
+    dothings<tbsla::cpp::MatrixELL>(input, format);
+  } else if(format == "DENSE" | format == "dense") {
+    dothings<tbsla::cpp::MatrixDENSE>(input, format);
+  } else {
+    std::cerr << format << " unrecognized!" << std::endl;
+    exit(1);
+  }
+
   return 0;
 }
