c27f95ab845c14e0e7f3190483bbc6a37f1c6a00
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2021-07-30 16:24:09 +0900

    scripts : example of submission command generation and compilation

---
+++ tools/bash/_gen_all.sh
@@ -0,0 +1,23 @@
+OP=Ax_
+OP=AAxpAxpx
+OP=page_rank
+matrix=nlpkkt200
+matrix=nlpkkt120
+matrix=cage14
+matrix=cage15
+
+#python tools/gen_submit_mm_cmd.py --OP $OP --Ns 1 --Ne 1 --machine Fugaku --matrixfolder ~/TBSLA/$matrix --matrixtype $matrix --numa-init --OMP
+python tools/gen_submit_mm_cmd.py --OP $OP --Ns 1 --Ne 2 --machine Fugaku --matrixfolder ~/TBSLA/$matrix --matrixtype $matrix --numa-init --MPI
+#python tools/gen_submit_mm_cmd.py --OP $OP --Ns 1 --Ne 2 --machine Fugaku --matrixfolder ~/TBSLA/$matrix --matrixtype $matrix --numa-init --MPIOMP
+
+
+#OP=Ax_
+#N=8000000
+#C=16
+#python tools/gen_submit_cmd.py --C $C --OP $OP --NR $N --NC $N --Ns 1 --Ne 1 --machine Fugaku --numa-init --OMP
+#python tools/gen_submit_cmd.py --C $C --OP $OP --NR $N --NC $N --Ns 1 --Ne 2 --machine Fugaku --numa-init --MPI
+#python tools/gen_submit_cmd.py --C $C --OP $OP --NR $N --NC $N --Ns 1 --Ne 2 --machine Fugaku --numa-init --MPIOMP
+#OP=AAxpAxpx
+#python tools/gen_submit_cmd.py --C $C --OP $OP --NR $N --NC $N --Ns 1 --Ne 1 --machine Fugaku --numa-init --OMP
+#python tools/gen_submit_cmd.py --C $C --OP $OP --NR $N --NC $N --Ns 1 --Ne 2 --machine Fugaku --numa-init --MPI
+#python tools/gen_submit_cmd.py --C $C --OP $OP --NR $N --NC $N --Ns 1 --Ne 2 --machine Fugaku --numa-init --MPIOMP
---
+++ tools/bash/build_fugaku_computenode_fujitsumpi.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+set -e
+
+export PATH=${INSTALL_DIR}/cmake/3.19.8/bin:$PATH
+
+rm -rf ${INSTALL_DIR}/tbsla
+#rm -rf _build
+mkdir -p _build
+cd _build
+
+export OMP_NUM_THREADS=4
+
+cmake -DTBSLA_ENABLE_VECTO=ON -DTBSLA_OpenMP_CXX_LIBRARIES="-Kopenmp" -DTBSLA_MPI_TESTS=ON -DCMAKE_CXX_FLAGS_RELEASE="-DNDEBUG" -DCMAKE_CXX_FLAGS="-Nclang -fPIC -Ofast -mcpu=native -funroll-loops -fno-builtin -march=armv8.2-a+sve" -DCMAKE_BUILD_TYPE=Release -DTBSLA_ENABLE_MPI=ON -DTBSLA_ENABLE_OMP=ON -DTBSLA_ENABLE_HPX=OFF -DMPI_CXX_COMPILER=mpiFCC -DCMAKE_CXX_COMPILER=mpiFCC -DCMAKE_INSTALL_PREFIX:PATH=$INSTALL_DIR/tbsla ..
+make -j VERBOSE=1
+make test
+make install
